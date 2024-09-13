eb25e159ea32e436ffa2a355650083b77e8c5929
Yang Wujie <wujieyang@gmail.com>
2024-09-13 16:57:22 +0800

    print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,34 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+static void
+dovmprint(pagetable_t pagetable, int depth)
+{
+  if (depth >= 3)
+    return;
+
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      for (int j = 0; j <= depth; j++) {
+        printf(" ..");
+      }
+      printf("%d: pte %p pa %p\n",i, pte, child);
+
+      dovmprint((pagetable_t)child, depth + 1);
+    }
+  }
+}
+
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n",pagetable);
+  dovmprint(pagetable, 0);
+}
